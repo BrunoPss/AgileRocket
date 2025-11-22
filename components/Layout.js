63027565bb0eeef7d977893e524057475d62013b
Bruno Fonseca <bruno.fonseca@onesource.pt>
2025-11-22 13:34:46 +0000

    add: chatbot integration

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,10 @@
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import Head from "next/head";
+import Script from "next/script";
+import dynamic from "next/dynamic";
+
+const Chatbot = dynamic(() => import("./Chatbot"), { ssr: false });
 
 export default function Layout({ children }) {
   return (
@@ -12,8 +16,12 @@ export default function Layout({ children }) {
         <link rel="alternate icon" href="/favicon.ico" />
         <meta name="theme-color" content="#6366F1" />
       </Head>
+
+      <Script src="https://webchat.ng.botschool.ai/chat-component.js" strategy="afterInteractive" />
+      
       <Navbar />
       <main className="min-h-screen flex-grow p-5">{children}</main>
+      <Chatbot />
       <Footer />
     </>
   );
